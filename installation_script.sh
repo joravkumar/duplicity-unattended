b04a5622ba831c58d5bc42cc5d52b7533c543716
joravkumar <jorav@kochartech.com>
2022-01-12 21:10:16 +0530

    feat: added - aws cred on config, minimal installed script

---
+++ installation_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash  
+echo "Enter the email of User: "  
+read EMAIL
+
+apt update -y
+apt upgrade -y
+apt install python3-pip -y
+apt install pipenv -y 
+pipenv install
+pipenv shell
+apt install duplicity -y 
+apt install python-yaml -y
+apt install gnupg -y
+echo "Generating GPG ID. Do NOT set a passphrase"
+gpg --full-generate-key --pinentry loopback # Do NOT set a passphrase
+gpg --armor --output pubkey.gpg --export $EMAIL
+gpg --armor --output privkey.gpg --export-secret-key $EMAIL
+mkdir /etc/duplicity-unattended
+cp duplicity-unattended /usr/bin/
+chmod +x /usr/bin/duplicity-unattended
+cp config.yaml /etc/duplicity-unattended/
+duplicity-unattended --config /etc/duplicity-unattended/config.yaml --dry-run
